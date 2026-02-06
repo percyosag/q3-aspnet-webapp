2e13d9360de7bccc20aea1cd6845dc02ac4fcc66
Percy Osunde <osundepo@my.centennialcollege.ca>
2026-02-06 17:07:04 -0500

    Initial commit - ASP.NET Core web app

---
+++ MycoreApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
+
+namespace MycoreApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            string dateTime = DateTime.Now.ToString("d", new CultureInfo("en-US"));
+            ViewData["TimeStamp"] = dateTime;
+        }
+    }
+
+}
